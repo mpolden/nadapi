b1bb732d41a4bc13a33c96414e206f89969a7eaf
Martin Polden <martin.polden@gmail.com>
2015-04-06 22:50:56 +0200

    Go 1.1 can't build lint or vet

--- lint.sh
+++ lint.sh
@@ -3,21 +3,20 @@
 set -e
 
 function lint() {
-    go get github.com/golang/lint/golint
+    go get -d -v github.com/golang/lint/golint
     golint ./...
 }
 
 function vet() {
     local -r flags="$1"
-    go get golang.org/x/tools/cmd/vet
+    go get -d -v golang.org/x/tools/cmd/vet
     go tool vet $flags $PWD
 }
 
 
 case "$TRAVIS_GO_VERSION" in
     1.1*)
-        # lint isn't compatible with 1.1
-        vet
+        echo "go ${TRAVIS_GO_VERSION} doesn't support lint or vet"
         ;;
     1.2*)
         lint
