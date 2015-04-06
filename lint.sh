f12697916c995e7fcdd89c0f0c040688cfdd618f
Martin Polden <martin.polden@gmail.com>
2015-04-06 22:53:41 +0200

    Remove -d flag

--- lint.sh
+++ lint.sh
@@ -3,20 +3,20 @@
 set -e
 
 function lint() {
-    go get -d -v github.com/golang/lint/golint
+    go get -v github.com/golang/lint/golint
     golint ./...
 }
 
 function vet() {
     local -r flags="$1"
-    go get -d -v golang.org/x/tools/cmd/vet
+    go get -v golang.org/x/tools/cmd/vet
     go tool vet $flags $PWD
 }
 
 
 case "$TRAVIS_GO_VERSION" in
     1.1*)
-        echo "go ${TRAVIS_GO_VERSION} doesn't support lint or vet"
+        printf "go ${TRAVIS_GO_VERSION} doesn't support lint or vet\n"
         ;;
     1.2*)
         lint
