a4bee708a212be9fa110d825153623d8795c6a7d
Martin Polden <martin.polden@gmail.com>
2016-01-15 17:42:47 +0100

    Persist state in simulated device

--- nad/nad_test_device.go
+++ nad/nad_test_device.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type testPort struct {
 	reply chan string
+	state map[string]Cmd
 }
 
 func (p *testPort) Close() (err error) {
@@ -17,61 +18,42 @@ func (p *testPort) Read(b []byte) (n int, err error) {
 }
 
 func (p *testPort) Write(b []byte) (n int, err error) {
-	cmd := string(b)
-	switch cmd {
-	case "\rMain.Model?\r":
-		p.reply <- "Main.Model=C356\n"
-	case "\rMain.Mute=On\r":
-		p.reply <- "\nMain.Mute=On\n"
-	case "\rMain.Mute=Off\r":
-		p.reply <- "\n\nMain.Mute=Off\n"
-	case "\rMain.Power=On\r":
-		p.reply <- "\n\n\nMain.Power=On\n"
-	case "\rMain.Power=Off\r":
-		p.reply <- "\nMain.Power=Off\n"
-	case "\rMain.Power?\r":
-		p.reply <- "\nMain.Power=On\n"
-	case "\rMain.Source=CD\r":
-		p.reply <- "\nMain.Source=CD\n"
-	case "\rMain.Source=TUNER\r":
-		p.reply <- "\nMain.Source=TUNER\n"
-	case "\rMain.Source=VIDEO\r":
-		p.reply <- "\nMain.Source=VIDEO\n"
-	case "\rMain.Source=DISC/MDC\r":
-		p.reply <- "\nMain.Source=DISC/MDC\n"
-	case "\rMain.Source=TAPE2\r":
-		p.reply <- "\nMain.Source=TAPE2\n"
-	case "\rMain.Source=AUX\r":
-		p.reply <- "\nMain.Source=AUX\n"
-	case "\rMain.Source?\r":
-		p.reply <- "\nMain.Source=CD\n"
-	case "\rMain.SpeakerA=On\r":
-		p.reply <- "\nMain.SpeakerA=On\n"
-	case "\rMain.SpeakerA=Off\r":
-		p.reply <- "\nMain.SpeakerA=Off\n"
-	case "\rMain.SpeakerA?\r":
-		p.reply <- "\nMain.SpeakerA=On\n"
-	case "\rMain.SpeakerB=On\r":
-		p.reply <- "\nMain.SpeakerB=On\n"
-	case "\rMain.SpeakerB=Off\r":
-		p.reply <- "\nMain.SpeakerB=Off\n"
-	case "\rMain.Tape1=On\r":
-		p.reply <- "\nMain.Tape1=On\n"
-	case "\rMain.Tape1=Off\r":
-		p.reply <- "\nMain.Tape1=Off\n"
-	case "\rMain.Volume+\r":
-		p.reply <- "\nMain.Volume+\n"
-	case "\rMain.Volume-\r":
-		p.reply <- "\nMain.Volume-\n"
-	default:
-		return 0, fmt.Errorf("unknown command: %q", cmd)
+	cmd, err := ParseCmd(string(b))
+	if err != nil {
+		return 0, err
 	}
+	if !cmd.Valid() {
+		return 0, fmt.Errorf("invalid command: %s", cmd.String())
+	}
+	if cmd.Operator == "?" {
+		r, ok := p.state[cmd.Variable]
+		if !ok {
+			return 0, fmt.Errorf("missing initial value for: %s", cmd.String())
+		}
+		p.reply <- r.Delimited()
+		return len(b), nil
+	}
+	p.state[cmd.Variable] = cmd
+	p.reply <- cmd.Delimited()
 	return len(b), nil
+}
+
+func newTestPort() *testPort {
+	reply := make(chan string, 1)
+	state := make(map[string]Cmd)
+	// Initial state
+	state["Model"] = Cmd{Variable: "Model", Operator: "=", Value: "C356"}
+	state["Mute"] = Cmd{Variable: "Mute", Operator: "=", Value: "Off"}
+	state["Power"] = Cmd{Variable: "Power", Operator: "=", Value: "Off"}
+	state["SpeakerA"] = Cmd{Variable: "SpeakerA", Operator: "=", Value: "On"}
+	state["SpeakerB"] = Cmd{Variable: "SpeakerB", Operator: "=", Value: "Off"}
+	state["Tape1"] = Cmd{Variable: "Tape1", Operator: "=", Value: "Off"}
+	state["Source"] = Cmd{Variable: "Tape1", Operator: "=", Value: "CD"}
+	return &testPort{reply: reply, state: state}
 }
 
 // NewTestClient creates a client that communicates with a simulated amp.
 func NewTestClient() *Client {
-	reply := make(chan string, 1)
-	port := &testPort{reply: reply}
+	port := newTestPort()
 	return &Client{port: port, EnableVolume: true}
 }
